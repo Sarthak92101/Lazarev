4512dbcc4e0d3beff66aa0a9e28442d2b27e6c7d
Sarthak Sharma <sarthaksharma25702@gmail.com>
2025-08-17 20:22:03 +0530

    New

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
+function locomotiveAnimation() {
+    gsap.registerPlugin(ScrollTrigger);
+
+    const locoScroll = new LocomotiveScroll({
+        el: document.querySelector("#main"),
+        smooth: true,
+
+        // for tablet smooth
+        tablet: { smooth: true },
+
+        // for mobile
+        smartphone: { smooth: true }
+    });
+    locoScroll.on("scroll", ScrollTrigger.update);
+
+    ScrollTrigger.scrollerProxy("#main", {
+        scrollTop(value) {
+            return arguments.length
+                ? locoScroll.scrollTo(value, 0, 0)
+                : locoScroll.scroll.instance.scroll.y;
+        },
+        getBoundingClientRect() {
+            return {
+                top: 0,
+                left: 0,
+                width: window.innerWidth,
+                height: window.innerHeight
+            };
+        }
+    });
+
+    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+    ScrollTrigger.refresh();
+}
 function navAnimation() {
     var nav = document.querySelector("nav")
 
@@ -39,8 +74,6 @@ function navAnimation() {
         })
     })
 }
-
-
 
 function Page2Animation() {
     var RightElemens = document.querySelectorAll(".right-elem");
@@ -92,23 +125,41 @@ function page3VideoAnimation() {
             borderRadius: "30px"
         })
     })
+
+
+    var sections = document.querySelectorAll(".sec-right");
+
+    sections.forEach(function (elem) {
+        elem.addEventListener("mouseenter", () => {
+            elem.childNodes[3].style.opacity = 1;
+            elem.childNodes[3].play();
+        })
+
+        elem.addEventListener("mouseleave", () => {
+            elem.childNodes[3].style.opacity = 0;
+            elem.childNodes[3].load();
+        })
+    })
+
 }
 
-var sections = document.querySelectorAll(".sec-right");
 
-sections.forEach(function (elem) {
-    elem.addEventListener("mouseenter", () => {
-        elem.childNodes[3].style.opacity = 1;
-        elem.childNodes[3].play();
-    })
-
-     elem.addEventListener("mouseleave", () => {
-        elem.childNodes[3].style.opacity = 0;
-        elem.childNodes[3].load();
-    })
+function page7Animation(){
+    gsap.from(".btm7 h4", {
+    x: 0,
+    duration: 1,
+    scrollTrigger: {
+        trigger: ".btm7",
+        scroller: "#main",
+        start: "top 80%",
+        end: "top 10%",
+        scrub: true
+    }
 })
-
-// navAnimation();
-// Page2Animation();
-// page3VideoAnimation();
+}
+locomotiveAnimation();
+navAnimation();
+Page2Animation();
+page3VideoAnimation();
+page7Animation();
 
